3e400d5fca14ca656befffbab2ebbd2f27a88838
unknown <qamarraza740@gmail.com>
2022-12-09 20:50:09 +0530

    code has been fixed by Kamar

--- prime.js
+++ prime.js
@@ -1,9 +1,21 @@
-function checkPrime(num){
-         for(let i=i; i<=num; i++){
-              if(num%i){
-                    return true;
+function checkPrime(number){
+
+         let factor=0;
+         for(let i=1; i<=number; i++){
+              if(number%i==0){
+                    factor++;
               }
          }
-         return false;
+         if (factor==2){
+             return true;
+         }
+             return false;
 }
-checkPrime(13);
+
+let answer=checkPrime(13);
+if (answer==true){
+       console.log("prime");
+}
+else {
+        console.log("not prime");
+}
